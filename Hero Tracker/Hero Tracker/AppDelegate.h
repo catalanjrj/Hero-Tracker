737d424d2ad942f20c7a718a3a8cc0cda1e51b8d
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-04-22 13:13:53 -0400

    started storyboard

---
+++ Hero Tracker/Hero Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hero Tracker
+//
+//  Created by Jorge Catalan on 4/22/16.
+//  Copyright © 2016 Jorge Catalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hero Tracker/Hero Tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hero Tracker
+//
+//  Created by Jorge Catalan on 4/22/16.
+//  Copyright © 2016 Jorge Catalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
